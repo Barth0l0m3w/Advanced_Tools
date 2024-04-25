02980ca8b5cb6083214ff9a5226f0cc476682140
Nathalie <xnhogenbergx@gmail.com>
2024-04-25 22:02:04 +0200

    1st proper test
    fps inconsistency gone. 1st full test run and code clean-up

--- Assets/scripts/InfoDump.cs
+++ Assets/scripts/InfoDump.cs
@@ -1,85 +1,79 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-using System.Text;
-using System.Threading;
 using NaughtyAttributes;
-using UnityEngine.Profiling;
-using Object = System.Object;
+using UnityEngine.Serialization;
 
 public class InfoDump : MonoBehaviour
 {
-    [SerializeField] private BreakUnity _breakUnity;
-    public string name;
-    private string filename;
-    private bool active;
-    private bool fileWritten;
-    private int average;
-
-    private int gridX;
-    private int gridY;
-
-    private float lastFrameGpuTime;
-    private float gpuTime;
-    private float currentFrameGpuTime;
-
-    [SerializeField] private int dataSample = 100;
-
-    private List<int> fpsList;
-
+    [SerializeField] private BreakUnity breakUnity;
+    [SerializeField] private int dataSample = 50;
+    public string nameCvs;
     public int testNumber;
 
+    private string _dataPath;
+
+    private bool _active;
+    private bool _fileWritten;
+
+    private int _average;
+    private int _gridX;
+    private int _gridY;
+
+    private float _lastFrameGpuTime;
+    private float _gpuTime;
+    private float _currentFrameGpuTime;
+
+    private List<int> _fpsList;
 
     private void Start()
     {
-        filename = Application.dataPath + "/" + name + ".csv";
+        _dataPath = Application.dataPath + "/" + nameCvs + ".csv";
+        _fpsList = new List<int>();
+        _active = true;
 
-        fpsList = new List<int>();
-        active = true;
-
-        TextWriter tw = new StreamWriter(filename, false);
+        //creating the file and filling in the headlines
+        TextWriter tw = new StreamWriter(_dataPath, false);
         tw.WriteLine("Test Number, ShadersTotal, FPS, GPU Usage");
         tw.Close();
     }
 
     private void Update()
     {
-        if (!active)
+        if (!_active)
         {
             return;
         }
 
-        if (fpsList.Count >= dataSample)
+        //calculating the frames per second
+        int fps = Mathf.RoundToInt(1f / Time.deltaTime);
+        _fpsList.Add(fps);
+
+        if (_fpsList.Count >= dataSample)
         {
             DataAverage();
         }
-
-        int fps = Mathf.RoundToInt(1f / Time.deltaTime);
-
+        
         //gpu usage
-        currentFrameGpuTime = Time.realtimeSinceStartup;
-        gpuTime = (currentFrameGpuTime - lastFrameGpuTime) * 1000; // Convert to milliseconds
+        _currentFrameGpuTime = Time.realtimeSinceStartup;
+        _gpuTime = (_currentFrameGpuTime - _lastFrameGpuTime) * 1000; // Convert to milliseconds
         // Update last frame GPU time
-        lastFrameGpuTime = currentFrameGpuTime;
-
-        fpsList.Add(fps);
+        _lastFrameGpuTime = _currentFrameGpuTime;
     }
 
     private void DataAverage()
     {
         int sum = 0;
 
-        foreach (var a in fpsList)
+        foreach (var a in _fpsList)
         {
             sum += a;
         }
 
-        average = sum / dataSample;
-        Debug.Log(average);
+        _average = sum / _fpsList.Count;
+        Debug.Log(_average);
 
-        active = false;
+        _active = false;
     }
 
     [Button]
@@ -87,21 +81,21 @@ public class InfoDump : MonoBehaviour
     {
         Debug.Log("write to file");
 
-        gridX = Mathf.FloorToInt(_breakUnity.size.x);
-        gridX = Mathf.FloorToInt(_breakUnity.size.y);
+        _gridX = Mathf.FloorToInt(breakUnity.size.x);
+        _gridX = Mathf.FloorToInt(breakUnity.size.y);
 
-        int shadersTotal = gridX * gridX;
+        int shadersTotal = _gridX * _gridX;
 
-        TextWriter tw = new StreamWriter(filename, true);
+        TextWriter tw = new StreamWriter(_dataPath, true);
 
         tw.WriteLine(testNumber + ", " + shadersTotal
-                     + ", " + average + ", " + gpuTime);
+                     + ", " + _average + ", " + _gpuTime);
         tw.Close();
 
         testNumber += 1;
-        fpsList.Clear(); //reset
-        active = true;
+        _fpsList.Clear(); //reset
+        _active = true;
 
-        _breakUnity.size = new Vector2(_breakUnity.size.x + 3, _breakUnity.size.y + 3); //update grid size
+        breakUnity.size = new Vector2(breakUnity.size.x + 3, breakUnity.size.y + 3); //update grid size
     }
 }
