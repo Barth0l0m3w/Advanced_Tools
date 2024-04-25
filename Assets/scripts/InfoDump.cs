237fbbb2285efce82f79f659033534cc9f6b469c
Nathalie <xnhogenbergx@gmail.com>
2024-04-25 21:01:15 +0200

    1st round of tests
    getting rid of some errors and inconstancies
    more intervals

--- Assets/scripts/InfoDump.cs
+++ Assets/scripts/InfoDump.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 using System.IO;
 using System.Text;
 using UnityEngine.Profiling;
+using Object = System.Object;
 
 public class InfoDump : MonoBehaviour
 {
     [SerializeField] private BreakUnity _breakUnity;
-
-    private string filename = "";
+    public string name;
+    private string filename;
     private bool active;
     private bool fileWritten;
     private int average;
 
     private int gridX;
     private int gridY;
-    
+
     private float lastFrameGpuTime;
     private float gpuTime;
     private float currentFrameGpuTime;
@@ -31,25 +32,22 @@ public class InfoDump : MonoBehaviour
 
     private void Start()
     {
-        
-        lastFrameGpuTime = Time.realtimeSinceStartup;
-        filename = Application.dataPath + "/test.csv";
+        filename = Application.dataPath + "/" + name + ".csv";
 
         fpsList = new List<int>();
         active = true;
-        
+
         TextWriter tw = new StreamWriter(filename, false);
-        tw.WriteLine("Test Number, ShadersTotal, FPS, GPU Usage");itk
+        tw.WriteLine("Test Number, ShadersTotal, FPS, GPU Usage");
         tw.Close();
     }
 
     private void Update()
     {
-        if ((Input.GetKeyDown(KeyCode.A)))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("write to file activated");
             WriteToFile();
-            //fileWritten = true;
         }
 
         if (!active)
@@ -63,21 +61,18 @@ public class InfoDump : MonoBehaviour
         }
 
         int fps = Mathf.RoundToInt(1f / Time.deltaTime);
-        
+
         //gpu usage
         currentFrameGpuTime = Time.realtimeSinceStartup;
         gpuTime = (currentFrameGpuTime - lastFrameGpuTime) * 1000; // Convert to milliseconds
         // Update last frame GPU time
         lastFrameGpuTime = currentFrameGpuTime;
-        
+
         fpsList.Add(fps);
     }
 
     private void DataAverage()
     {
-        gridX = Mathf.FloorToInt(_breakUnity.size.x);
-        gridX = Mathf.FloorToInt(_breakUnity.size.y);
-        
         StringBuilder builder = new();
         int sum = 0;
 
@@ -85,23 +80,27 @@ public class InfoDump : MonoBehaviour
         {
             sum += a;
         }
-        
+
         Debug.Log(gpuTime);
-        
+
         average = sum / dataSample;
         active = false;
     }
 
     private void WriteToFile()
     {
+        gridX = Mathf.FloorToInt(_breakUnity.size.x);
+        gridX = Mathf.FloorToInt(_breakUnity.size.y);
+
         int shadersTotal = gridX * gridX;
-        
+
         TextWriter tw = new StreamWriter(filename, true);
 
         tw.WriteLine(testNumber + ", " + shadersTotal
                      + ", " + average + ", " + gpuTime);
         tw.Close();
-        
+
+        gpuTime = 0;
         testNumber += 1;
         fpsList.Clear(); //reset
         active = true;
